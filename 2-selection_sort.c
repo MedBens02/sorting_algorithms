b6a634372b7489472ae5b645fad0e34c0060662f
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-19 12:26:23 -0800

    2

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sort an array of ints using selection sort
+ * @array: the array to sort
+ * @size: its size
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int *idx, tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size + 1; i++)
+	{
+		idx = i;
+
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[idx])
+				idx = j;
+
+		if (idx != i)
+		{
+			tmp = array[idx];
+			array[idx] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
